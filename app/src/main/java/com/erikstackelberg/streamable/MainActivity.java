2a6c390904ea5c799967691942411b15ea1d8eac
Erik Stackelberg <erikstackelberg@gmail.com>
2017-08-23 16:28:39 +0200

    fix for mistakenly removing options menu inflation

--- app/src/main/java/com/erikstackelberg/streamable/MainActivity.java
+++ app/src/main/java/com/erikstackelberg/streamable/MainActivity.java
@@ -18,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             //tbi.
             return true;
